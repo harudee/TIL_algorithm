d3eb85317d7be301a15047073032bdefa6885bb3
hakyung kim <83569577+harudee@users.noreply.github.com>
2023-10-19 12:13:46 +0900

    [Bronze III] Title: 오븐 시계, Time: 176 ms, Memory: 22484 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)){
+    
+    val str = nextLine()
+    val time = nextInt()
+    
+    val arr = str!!.split(" ").map{it.toInt()}
+    
+    val h = arr[0]
+    val m = arr[1]
+    
+    var resultH =0
+    var resultM =0
+    val calTime = m+time
+    
+    if((calTime%60)==0){
+        resultM = 0
+        
+    } else{
+        resultM = calTime%60
+        
+    }
+    
+    resultH = h + (calTime/60)
+    if(resultH >= 24){
+        resultH -= 24
+    }
+    
+    println("$resultH $resultM")
+    
+}
